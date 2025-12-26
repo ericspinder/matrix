4cb0154adab7c91fdeac36193cdede810fdef09d
Eric Pinder <ericspinder@gmail.com>
2025-12-26 01:29:30 -0500

    updates

---
+++ src/main/java/dev/inward/matrix/control/terrene/Hesiod.java
@@ -0,0 +1,30 @@
+/*
+ *  Pinder's Matrix © 2025. by Eric S Pinder is licensed under Creative Commons Attribution-NonCommercial-ShareAlike 4.0 International. To view a copy of this license, visit https://creativecommons.org/licenses/by-nc-sa/4.0/
+ */
+
+package dev.inward.matrix.control.terrene;
+
+import java.io.IOException;
+import java.net.URL;
+import java.net.URLConnection;
+
+public class Hesiod extends Terrene {
+
+    public Hesiod() {
+        super("HS", "hesiod");
+    }
+    @Override
+    public String getName() {
+        return label;
+    }
+
+    @Override
+    public TerreneModel getModel() {
+        return null;
+    }
+
+    @Override
+    protected URLConnection openConnection(URL u) throws IOException {
+        return null;
+    }
+}
